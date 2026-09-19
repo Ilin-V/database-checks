146e3a528e19fd602d2b9eb167d120daf252b21e
Вадим <ilinv2448@email.com>
2026-09-19 10:21:37 -0700

    ну чето

---
+++ Python Fundamentals/phonebook.py
@@ -0,0 +1,36 @@
+#Меню
+def menu():
+    print("Добавить / обновить")
+    print("Найти по имени")
+    print("Показать все")
+    print("Выход")
+    action = input("Что хотите выбрать: ")
+    return action
+#Словарь
+phonebook = {"Вадим" : "+7 937 395 94 60"}
+#Цикл
+while True:
+    action = menu()
+    #Добавить/обновить
+    if action.lower() == "добавить" or action.lower() == "обновить":
+        name = input("Введите имя: ")
+        number = input("Введите номер: ")
+        phonebook[name] = number
+        print(phonebook)
+    #Найти по имени
+    if action.lower() == "найти по имени" or action.lower() == "найти":
+        print(phonebook)
+        name = input("Чей номер хотите найти?: ")
+        if name in phonebook:
+            name = phonebook[name]
+            print(name)
+        else:
+            print("Контакта нет")
+    #Показать все
+    if action.lower() == "показать все" or action.lower() == "показать":
+        print(phonebook)
+    #Выход
+    if action.lower() == "выход":
+        print("Вы вышли!")
+        break
+    print()
