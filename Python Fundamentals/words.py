2e629c378c5e56185f8ffff7b598fda1ef0c7aac
Вадим <ilinv2448@email.com>
2026-09-19 06:57:34 -0700

    рабочий код

--- Python Fundamentals/words.py
+++ Python Fundamentals/words.py
@@ -1,1 +1,17 @@
-words = input()
+words = input("Введите строку: ")
+text = words.split()
+#Сколько слов
+count_words = 0
+for i in text:
+    count_words += 1
+print(count_words)
+
+#Самое длинное слово
+length = ""
+for i in text:
+    if len(length) < len(i):
+        length = i
+print(length)
+
+#Cтроку в нижнем регистре
+print(words.lower())
