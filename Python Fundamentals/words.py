2c554cb5615bb7d0a935b652e3bf917bfc6e24e2
Вадим <ilinv2448@email.com>
2026-09-19 08:01:17 -0700

    Добавление функции

--- Python Fundamentals/words.py
+++ Python Fundamentals/words.py
@@ -1,17 +1,24 @@
 words = input("Введите строку: ")
 text = words.split()
 #Сколько слов
-count_words = 0
-for i in text:
-    count_words += 1
-print(count_words)
+def words_many(text):
+    count_words = 0
+    for i in text:
+        count_words += 1
+    return count_words
+count_words = words_many(text)
 
 #Самое длинное слово
-length = ""
-for i in text:
-    if len(length) < len(i):
-        length = i
-print(length)
+def len_word(text):
+    length = ""
+    for i in text:
+        if len(length) < len(i):
+            length = i
+    return length
+length = len_word(text)
 
+#Вывод
+print(f"Сколько слов: {count_words}")
+print(f"Самое длинное слово: {length}")
 #Cтроку в нижнем регистре
 print(words.lower())
