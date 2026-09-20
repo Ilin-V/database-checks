abbd8844384d6f598c3049fefc40e87462907248
Вадим <ilinv2448@email.com>
2026-09-20 07:25:23 -0700

    исправления бага с работай регистром

--- Python Fundamentals/stats.py
+++ Python Fundamentals/stats.py
@@ -3,11 +3,11 @@ def count_letters(text: str) -> dict:
     dictionary = {}
     for i in text:
         letter = i.lower()
-        if letter.isalpha():
+        if letter.lower().isalpha():
             if letter not in dictionary:
-                dictionary[i] = 1
+                dictionary[i.lower()] = 1
             else :
-                dictionary[i] +=1
+                dictionary[i.lower()] +=1
     return dictionary
 result = count_letters(text)
 print(result)
